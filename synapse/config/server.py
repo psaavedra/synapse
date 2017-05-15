39b34310832bc1f08b1d999819dd5afe601ea3de
Pablo Saavedra <saavedra.pablo@gmail.com>
2017-05-15 17:25:51 +0200

    Fixed a pep8 complain

--- synapse/config/server.py
+++ synapse/config/server.py
@@ -169,6 +169,10 @@ class ServerConfig(Config):
         # The GC threshold parameters to pass to `gc.set_threshold`, if defined
         # gc_thresholds: [700, 10, 10]
 
+        # The default behavior of created room if m.federate is not
+        # specified during the room creation
+        # room_federate_default = True
+
         # Set the limit on the returned events in the timeline in the get
         # and sync operations. The default value is -1, means no upper limit.
         # filter_timeline_limit: 5000
